bbe549f59ad4e09b1d62b2071ca61b3552f62542
fengdanhui <602292747@qq.com>
2020-04-30 17:20:06 +0800

    2020/4/30

--- imooc-security/imooc-security-demo/src/main/java/com/imooc/web/config/WebConfig.java
+++ imooc-security/imooc-security-demo/src/main/java/com/imooc/web/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.imooc.web.config;
 
 import com.imooc.web.filter.TimeFilter;
+import com.imooc.web.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,20 +18,29 @@ import java.util.List;
  * 2020/4/20
  */
 @Configuration
-public class WebConfig {
-    @Bean
-    public FilterRegistrationBean timeFilter() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        TimeFilter timeFilter = new TimeFilter();
-        registrationBean.setFilter(timeFilter);
-        /**
-         * 配置方式，指定filter在哪些url时起作用
-         */
-        List<String> urls = new ArrayList<>();
-        //所有url都会起作用
-        urls.add("/*");
-        registrationBean.setUrlPatterns(urls);
-        return registrationBean;
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
     }
+
+//    @Bean
+//    public FilterRegistrationBean timeFilter() {
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//        TimeFilter timeFilter = new TimeFilter();
+//        registrationBean.setFilter(timeFilter);
+//        /**
+//         * 配置方式，指定filter在哪些url时起作用
+//         */
+//        List<String> urls = new ArrayList<>();
+//        //所有url都会起作用
+//        urls.add("/*");
+//        registrationBean.setUrlPatterns(urls);
+//        return registrationBean;
+//
+//    }
 }
